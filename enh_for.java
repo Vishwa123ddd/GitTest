cc4c8a94c210bbb9a691e3f6585db5ea305c1955
Vishwa123ddd <86395928+Vishwa123ddd@users.noreply.github.com>
2021-06-24 10:38:23 +0530

    Add files via upload

---
+++ enh_for.java
@@ -0,0 +1,22 @@
+
+import java.util.*;
+class test{
+public static void main(String args[])
+{  
+
+int arr[][]={{1,2,3},{4,5,6}};
+for(int temp[]:arr)
+{
+	for(int data:temp)
+	{
+	System.out.print(data+"  ");
+	}
+System.out.println("\n");
+
+
+}
+ 
+System.out.println("");
+
+}
+}
